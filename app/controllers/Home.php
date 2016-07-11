5e3bb4796023ef4eebf50a40e810d46124cf04d8
itsshady101 <rocks.ayush2011@gmail.com>
2016-07-11 18:23:13 +0530

    Eloquent added to make models easy and extensible

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,10 +1,20 @@
 <?php
 
 class Home extends Controller {
+	
     public function index($name = []) {
         $user = $this->model('User');
         $user->name = $name[0];
         
         $this->view('home/index', ['name' => $user->nameṬ]);
+    }
+
+    // Not to be used in realworld
+    // In real world creating is done by Post Request not Get =D
+    public function create($details = []) {
+    	User::create([
+    		'username' => $details[0],
+    		'email' => $details[1]
+    	]);
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,9 @@
 <?php
 
-class User extends Controller {
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
+class User extends Eloquent {
+	public $name;
+
+	protected $fillable = ['username', 'email'];
 }
